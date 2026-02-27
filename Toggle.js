50b52c1bd8ca21cec8bb9fd9799eadeeca5023b9
Atul-Chavan-3755 <atulchavan200@gmail.com>
2026-02-27 15:42:14 +0530

    Toggle Button

---
+++ Toggle.js
@@ -0,0 +1,16 @@
+let modebtn = document.querySelector("#mode");
+let mode = "light";
+let body = document.querySelector("body")
+
+modebtn.addEventListener("click", () =>{
+  if (mode === "light") {
+    mode = "dark";
+    body.classList.add("dark");
+    body.classList.remove("light");
+  } else {
+    mode = "light";
+    body.classList.add("light");
+    body.classList.remove("dark");
+    }
+  console.log(mode);
+});
